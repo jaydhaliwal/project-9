2e1f543a480a9a50b9116835b6afbd659797c2a4
jaydhaliwal <94186548+jaydhaliwal@users.noreply.github.com>
2022-01-04 10:55:14 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var Box;
+
+function setup(){
+createCanvas(400,400)
+box = createSprite(200,200,30,30)
+
+}
+  
+  function draw() 
+  {
+  background(30);
+
+if(keyIsDown(RIGHT_ARROW))
+{
+  background ("red")
+}
+
+if(keyIsDown(LEFT_ARROW))
+{
+  background ("blue")
+}
+if(keyIsDown(UP_ARROW))
+{
+  background ("green")
+}
+
+if(keyIsDown(DOWN_ARROW))
+{
+  background ("orange")
+}
+drawSprites()
+
+}
